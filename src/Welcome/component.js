c5e53aef5a1c44016a75e57bd6fa676a6ea26a8d
Chris Eisenbraun <chris@chriseisenbraun.com>
2018-12-16 19:35:13 -0800

    Welcome section styling

--- src/Welcome/component.js
+++ src/Welcome/component.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import './Welcome.css'
 import {MissionCarousel} from '../MissionCarousel';
 import {MissionDetail} from '../MissionDetail';
@@ -6,21 +6,28 @@ import {MissionDetail} from '../MissionDetail';
 export class Welcome extends Component {
   render() {
     const {missions} = this.props;
-    return (
-      <>
-      <div className="welcome">
-        <h1 className="welcome__heading">Welcome to your sales onboarding experience</h1>
-        <div className="welcome-flex">
-        <p className="welcome__text welcome-flex-item">Welcome to your online onboarding experience. You will find your enablement with a click of the button below. The Missions are meant to be experienced in order, and are filled with brief and relevant information in a variety of formats. You will then go back to doing your job until you are ready to return for more of the experience. We hope this path is helpful for you to have some quick success in your role!</p>
+    return ( <> <div className="welcome">
+      <h1 className="welcome__heading">Welcome to your sales onboarding experience</h1>
+      <div className="welcome-flex">
+        <p className="welcome__text welcome-flex-item">Welcome to your online onboarding
+          experience. You will find your enablement with a click of the button below. The
+          Missions are meant to be experienced in order, and are filled with brief and
+          relevant information in a variety of formats. You will then go back to doing
+          your job until you are ready to return for more of the experience. We hope this
+          path is helpful for you to have some quick success in your role!</p>
         <div className="welcome__video welcome-flex-item">
-        <p>VIDEO PLAYER</p>
-
+          <iframe
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/DdSBxX3Gmjo"
+            frameborder="0"
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen></iframe>
         </div>
       </div>
-      </div>
-        <MissionCarousel missions={missions}/>
-        <MissionDetail missions={missions} />
-      </>
+    </div>
+    <MissionCarousel missions = {missions} /> <MissionDetail missions={missions}/>
+    </>
     )
   }
 }
