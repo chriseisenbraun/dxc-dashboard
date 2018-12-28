57199deb9b8669f2c2fefdeaaa4d8108ab9f431d
Chris Eisenbraun <chris@chriseisenbraun.com>
2018-12-27 23:25:57 -0800

    Quick style of checkpoints for demo

--- src/Checkpoint/component.js
+++ src/Checkpoint/component.js
@@ -1,42 +1,126 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import './checkpoint.css'
+
+//#2e8c5a
 
 export class Checkpoint extends Component {
   render() {
 
-      return (
-        <div className="container checkpoint">
+    return (
+      <div className="container checkpoint">
         <div className="row d-flex justify-content-between">
 
+            <div className="col checkpoint-icon-wrap">
+              <svg className="checkpoint-icon"
+                viewBox="0 0 300 300"
+                xmlns="http://www.w3.org/2000/svg"
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                stroke-linejoin="round"
+                stroke-miterlimit="1.414"><circle
+                cx="7099"
+                cy="798"
+                r="80"
+                fill="#2e8c5a"
+                transform="translate(-13160.627 -1346.246) scale(1.875)"/><path
+                d="M112.498 146.316c3.944-.025 8.587.094 13.6.488 15.237 1.194 31.331 9.45 46.125 9.45 13.106 0 27.775-7.031 27.775-7.031V81.254s-16.013 9.375-28.65 9.375c-16.856 0-33.719-9.375-50.575-9.375-6.925 0-20.775 2.344-20.775 2.344V212.51c0 3.5 2.8 6.244 6.25 6.244a6.231 6.231 0 0 0 6.25-6.244v-66.194z"
+                fill="#fff"/></svg>
 
-        <div className="checkpoint-icon">
-        <svg viewBox="0 0 300 300" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="1.414"><circle cx="7099" cy="798" r="80" fill="#2c4a62" transform="translate(-13160.627 -1346.246) scale(1.875)"/><path d="M112.498 146.316c3.944-.025 8.587.094 13.6.488 15.237 1.194 31.331 9.45 46.125 9.45 13.106 0 27.775-7.031 27.775-7.031V81.254s-16.013 9.375-28.65 9.375c-16.856 0-33.719-9.375-50.575-9.375-6.925 0-20.775 2.344-20.775 2.344V212.51c0 3.5 2.8 6.244 6.25 6.244a6.231 6.231 0 0 0 6.25-6.244v-66.194z" fill="#fff"/></svg>
+            </div>
 
-        </div>
+            <div className="col checkpoint-icon-wrap">
+              <svg className="checkpoint-icon"
+                viewBox="0 0 300 300"
+                xmlns="http://www.w3.org/2000/svg"
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                stroke-linejoin="round"
+                stroke-miterlimit="1.414"><circle
+                cx="7099"
+                cy="798"
+                r="80"
+                fill="#ccc"
+                transform="translate(-13160.627 -1346.246) scale(1.875)"/><path
+                d="M112.498 146.316c3.944-.025 8.587.094 13.6.488 15.237 1.194 31.331 9.45 46.125 9.45 13.106 0 27.775-7.031 27.775-7.031V81.254s-16.013 9.375-28.65 9.375c-16.856 0-33.719-9.375-50.575-9.375-6.925 0-20.775 2.344-20.775 2.344V212.51c0 3.5 2.8 6.244 6.25 6.244a6.231 6.231 0 0 0 6.25-6.244v-66.194z"
+                fill="#fff"/></svg>
 
-         <div className="checkpoint-icon">
-        <svg viewBox="0 0 300 300" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="1.414"><circle cx="7099" cy="798" r="80" fill="#2c4a62" transform="translate(-13160.627 -1346.246) scale(1.875)"/><path d="M112.498 146.316c3.944-.025 8.587.094 13.6.488 15.237 1.194 31.331 9.45 46.125 9.45 13.106 0 27.775-7.031 27.775-7.031V81.254s-16.013 9.375-28.65 9.375c-16.856 0-33.719-9.375-50.575-9.375-6.925 0-20.775 2.344-20.775 2.344V212.51c0 3.5 2.8 6.244 6.25 6.244a6.231 6.231 0 0 0 6.25-6.244v-66.194z" fill="#fff"/></svg>
+            </div>
 
-        </div>
+            <div className="col checkpoint-icon-wrap">
+              <svg className="checkpoint-icon"
+                viewBox="0 0 300 300"
+                xmlns="http://www.w3.org/2000/svg"
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                stroke-linejoin="round"
+                stroke-miterlimit="1.414"><circle
+                cx="7099"
+                cy="798"
+                r="80"
+                fill="#ccc"
+                transform="translate(-13160.627 -1346.246) scale(1.875)"/><path
+                d="M112.498 146.316c3.944-.025 8.587.094 13.6.488 15.237 1.194 31.331 9.45 46.125 9.45 13.106 0 27.775-7.031 27.775-7.031V81.254s-16.013 9.375-28.65 9.375c-16.856 0-33.719-9.375-50.575-9.375-6.925 0-20.775 2.344-20.775 2.344V212.51c0 3.5 2.8 6.244 6.25 6.244a6.231 6.231 0 0 0 6.25-6.244v-66.194z"
+                fill="#fff"/></svg>
 
-         <div className="checkpoint-icon">
-        <svg viewBox="0 0 300 300" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="1.414"><circle cx="7099" cy="798" r="80" fill="#2c4a62" transform="translate(-13160.627 -1346.246) scale(1.875)"/><path d="M112.498 146.316c3.944-.025 8.587.094 13.6.488 15.237 1.194 31.331 9.45 46.125 9.45 13.106 0 27.775-7.031 27.775-7.031V81.254s-16.013 9.375-28.65 9.375c-16.856 0-33.719-9.375-50.575-9.375-6.925 0-20.775 2.344-20.775 2.344V212.51c0 3.5 2.8 6.244 6.25 6.244a6.231 6.231 0 0 0 6.25-6.244v-66.194z" fill="#fff"/></svg>
+            </div>
 
-        </div>
+            <div className="col checkpoint-icon-wrap">
+              <svg className="checkpoint-icon"
+                viewBox="0 0 300 300"
+                xmlns="http://www.w3.org/2000/svg"
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                stroke-linejoin="round"
+                stroke-miterlimit="1.414"><circle
+                cx="7099"
+                cy="798"
+                r="80"
+                fill="#ccc"
+                transform="translate(-13160.627 -1346.246) scale(1.875)"/><path
+                d="M112.498 146.316c3.944-.025 8.587.094 13.6.488 15.237 1.194 31.331 9.45 46.125 9.45 13.106 0 27.775-7.031 27.775-7.031V81.254s-16.013 9.375-28.65 9.375c-16.856 0-33.719-9.375-50.575-9.375-6.925 0-20.775 2.344-20.775 2.344V212.51c0 3.5 2.8 6.244 6.25 6.244a6.231 6.231 0 0 0 6.25-6.244v-66.194z"
+                fill="#fff"/></svg>
 
-         <div className="checkpoint-icon">
-        <svg viewBox="0 0 300 300" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="1.414"><circle cx="7099" cy="798" r="80" fill="#2c4a62" transform="translate(-13160.627 -1346.246) scale(1.875)"/><path d="M112.498 146.316c3.944-.025 8.587.094 13.6.488 15.237 1.194 31.331 9.45 46.125 9.45 13.106 0 27.775-7.031 27.775-7.031V81.254s-16.013 9.375-28.65 9.375c-16.856 0-33.719-9.375-50.575-9.375-6.925 0-20.775 2.344-20.775 2.344V212.51c0 3.5 2.8 6.244 6.25 6.244a6.231 6.231 0 0 0 6.25-6.244v-66.194z" fill="#fff"/></svg>
+            </div>
 
-        </div>
+            <div className="col checkpoint-icon-wrap">
+              <svg className="checkpoint-icon"
+                viewBox="0 0 300 300"
+                xmlns="http://www.w3.org/2000/svg"
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                stroke-linejoin="round"
+                stroke-miterlimit="1.414"><circle
+                cx="7099"
+                cy="798"
+                r="80"
+                fill="#ccc"
+                transform="translate(-13160.627 -1346.246) scale(1.875)"/><path
+                d="M112.498 146.316c3.944-.025 8.587.094 13.6.488 15.237 1.194 31.331 9.45 46.125 9.45 13.106 0 27.775-7.031 27.775-7.031V81.254s-16.013 9.375-28.65 9.375c-16.856 0-33.719-9.375-50.575-9.375-6.925 0-20.775 2.344-20.775 2.344V212.51c0 3.5 2.8 6.244 6.25 6.244a6.231 6.231 0 0 0 6.25-6.244v-66.194z"
+                fill="#fff"/></svg>
 
-         <div className="checkpoint-icon">
-        <svg viewBox="0 0 300 300" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="1.414"><circle cx="7099" cy="798" r="80" fill="#2c4a62" transform="translate(-13160.627 -1346.246) scale(1.875)"/><path d="M112.498 146.316c3.944-.025 8.587.094 13.6.488 15.237 1.194 31.331 9.45 46.125 9.45 13.106 0 27.775-7.031 27.775-7.031V81.254s-16.013 9.375-28.65 9.375c-16.856 0-33.719-9.375-50.575-9.375-6.925 0-20.775 2.344-20.775 2.344V212.51c0 3.5 2.8 6.244 6.25 6.244a6.231 6.231 0 0 0 6.25-6.244v-66.194z" fill="#fff"/></svg>
+            </div>
 
-        </div>
+          <div className="col checkpoint-icon-wrap">
+            <svg className="checkpoint-icon"
+              viewBox="0 0 300 300"
+              xmlns="http://www.w3.org/2000/svg"
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              stroke-linejoin="round"
+              stroke-miterlimit="1.414"><circle
+                cx="7099"
+                cy="798"
+                r="80"
+                fill="#ccc"
+                transform="translate(-13160.627 -1346.246) scale(1.875)" /><path
+                d="M112.498 146.316c3.944-.025 8.587.094 13.6.488 15.237 1.194 31.331 9.45 46.125 9.45 13.106 0 27.775-7.031 27.775-7.031V81.254s-16.013 9.375-28.65 9.375c-16.856 0-33.719-9.375-50.575-9.375-6.925 0-20.775 2.344-20.775 2.344V212.51c0 3.5 2.8 6.244 6.25 6.244a6.231 6.231 0 0 0 6.25-6.244v-66.194z"
+                fill="#fff" /></svg>
 
           </div>
+
         </div>
-      )
+      </div>
+    )
 
   }
 }
