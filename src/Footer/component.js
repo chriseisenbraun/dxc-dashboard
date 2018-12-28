b7dc22f52b87f42912972bfe286feece5deb8ba6
Chris Eisenbraun <chris@chriseisenbraun.com>
2018-12-27 23:02:40 -0800

    Add dummy text and style

--- src/Footer/component.js
+++ src/Footer/component.js
@@ -9,10 +9,10 @@ export class Footer extends Component {
         <div class="container">
           <div className="row">
             <div className="col">
-              <span className="text-light">Left</span>
+              <span className="footer-text">Oxygen8</span>
             </div>
             <div className="col text-right">
-              <span className="text-light">Right</span>
+              <span className="footer-text">&copy; 2019 oxygen all rights reserved</span>
             </div>
           </div>
         </div>
